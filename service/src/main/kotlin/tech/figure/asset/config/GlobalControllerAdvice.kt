5a612b837ca363672929395bde5dc4654d7d7662
kherzinger-figure <kherzinger@figure.com>
2021-09-14 13:07:05 -0700

    Disable WebFlux

--- service/src/main/kotlin/tech/figure/asset/config/GlobalControllerAdvice.kt
+++ service/src/main/kotlin/tech/figure/asset/config/GlobalControllerAdvice.kt
@@ -3,11 +3,11 @@ package tech.figure.asset.config
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.web.bind.annotation.ControllerAdvice
-// import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice(basePackages = ["tech.figure.asset.web"])
 @Order(Ordered.HIGHEST_PRECEDENCE)
-class GlobalControllerAdvice /*: ResponseEntityExceptionHandler()*/ {
+class GlobalControllerAdvice : ResponseEntityExceptionHandler() {
 
     // TODO
 
