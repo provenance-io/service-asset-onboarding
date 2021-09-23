e9182f0878744acf346d5ae51a01fe6da8d680c8
kherzinger-figure <kherzinger@figure.com>
2021-09-23 12:46:53 -0700

    Missing JODA money we are.

--- service/src/main/kotlin/tech/figure/asset/extensions/ObjectMapperExtensions.kt
+++ service/src/main/kotlin/tech/figure/asset/extensions/ObjectMapperExtensions.kt
@@ -6,11 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.figure.data.json.JodaMoneyModule
 import com.hubspot.jackson.datatype.protobuf.ProtobufModule
 
 fun ObjectMapper.configureFigureTech(): ObjectMapper = registerKotlinModule()
     .registerModule(JavaTimeModule())
-    //.registerModule(JodaMoneyModule())
+    .registerModule(JodaMoneyModule())
     .registerModule(ProtobufModule())
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     .setSerializationInclusion(JsonInclude.Include.NON_NULL)
